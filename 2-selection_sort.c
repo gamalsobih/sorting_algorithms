6801c892fb71deaf75d1982f7773f21ed18447f3
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-12-19 15:44:36 -0800

    tsk 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of ints
+ * @array: array of ints
+ * @size: size of the array
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp = 0;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+
+			print_array(array, size);
+		}
+	}
+}
