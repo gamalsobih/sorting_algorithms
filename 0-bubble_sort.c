1d107d8b02ba08477b04ff8470a187ad2e7e3927
gamal sob <133385330+gamalsobih@users.noreply.github.com>
2023-12-19 04:29:04 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,25 +9,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
+	int t;
 	size_t i, k;
 
-	if (!array || !size)
-		return;
-
-	i = 0;
-	while (i < size)
-	{
-		for (k = 0; k < size - 1; k++)
+	i=0;
+	while(i<size)
 		{
-			if (array[k] > array[k + 1])
-			{
-				temp = array[k];
-				array[k] = array[k + 1];
-				array[k + 1] = temp;
-				print_array(array, size);
-			}
+			for(k=0;k<size;k++)
+				{
+					if(array[k]>array[k+1])
+					{
+						t=array[k];
+						array[k]=array[k+1];
+						array[k+1]=t;
+					}
+				}
+			i++;
+			
 		}
-		i++;
-	}
+	
 }
