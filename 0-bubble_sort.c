16a9ee80e60213a3b1a62c915d43283b9d217610
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-12-19 14:36:59 -0800

    update task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,30 +9,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int t,s;
+	int t, s;
 	size_t i, k;
-	k=0;
-	i=size;
-	s=1;
-	while(s==1)
+
+	k = 0;
+	i = size;
+	s = 1;
+	while (s == 1)
+	{
+		s = 0;
+		while (k < i - 1)
 		{
-			s=0;
-			while(k<i-1)
-				{	
-					if(array[k]>array[k+1])
-					{
-						t=array[k];
-						array[k]=array[k+1];
-						array[k+1]=t;
-						s=1;
-						print_array(array, size);
-					}
-					
-					k++;
-				}
-			i=k;
-			k=0;
-			
+			if (array[k] > array[k + 1])
+			{
+				t = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = t;
+				s = 1;
+				print_array(array, size);
+			}
+			k++;
 		}
-	
+		i = k;
+		k = 0;
+	}
 }
