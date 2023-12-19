24166b6c9b6b3dad0258fbffa1d56fa20fb9fdcc
gamalsobih <gamalsob@gmail.com>
2023-12-18 18:43:47 -0800

    this is  a commit

--- sort.h
+++ sort.h
@@ -1,11 +1,10 @@
-#ifndef _sort.h_
-#define _sort.h_
+#ifndef _SORT_H
+#define _SORT_H
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,9 +14,30 @@ void bubble_sort(int *array, size_t size);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-#endif /* sort */
+
+/* print.c */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void check_list(const listint_t *list);
+void check_array(const int *array, size_t size);
+int *rand_array(int len, int max);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif
