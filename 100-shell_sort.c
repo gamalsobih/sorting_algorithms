836833505e8fb2a9fe82a37d79b22039559bc771
gamal sob <133385330+gamalsobih@users.noreply.github.com>
2023-12-20 03:58:56 +0200

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,64 +1,46 @@
 #include "sort.h"
-
+#include <stdio.h>
 /**
- * swap - swaps two nodes
- * @head: head of the list
- * @node1: first node to sort
- * @node2: second node to sort
+ * _swap - swaped 2 values.
+ * @array: the array for swap him values.
+ * @i: First index
+ * @j: Second index
+ * Return: Nothing
  */
-void swap(listint_t **head, listint_t *node1, listint_t *node2)
+void _swap(int *array, int i, int j)
 {
-	listint_t *prev, *next;
+	int tmp;
 
-	prev = node1->prev;
-	next = node2->next;
-
-	if (prev != NULL)
-		prev->next = node2;
-	else
-		*head = node2;
-	node1->prev = node2;
-	node1->next = next;
-	node2->prev = prev;
-	node2->next = node1;
-	if (next)
-		next->prev = node1;
+	if (array[i] != array[j])
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
 }
-/**
- * cocktail_sort_list - sorts a list using the cocktail sort algorithm
- * @list: list to sort
- */
-void cocktail_sort_list(listint_t **list)
-{
-	listint_t *head;
-	int flag = 0;
 
-	if (!list || !*list || !(*list)->next)
+/**
+ * shell_sort - sort the list and print the changes
+ * @array: The array to sort.
+ * @size: Size of the array
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t h = 0,  i, j;
+
+	if (size < 2)
 		return;
 
-	do {
-		for (head = *list; head->next != NULL; head = head->next)
-		{
-			if (head->n > head->next->n)
-			{
-				swap(list, head, head->next);
-				print_list(*list);
-				flag = 1;
-				head = head->prev;
-			}
-		}
-		if (flag == 0)
-			break;
-		flag = 0;
-		for (; head->prev != NULL; head = head->prev)
-		{
-			if (head->n < head->prev->n)
-			{
-				swap(list, head->prev, head);
-				print_list(*list);
-				flag = 1;
-				head = head->next;
-			}
-		}
-	} while (flag == 1);
+	while (h <= size / 3)
+		h = h * 3 + 1;
+
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
+				_swap(array, j, j - h);
+		h /= 3;
+		print_array(array, size);
+	}
 }
